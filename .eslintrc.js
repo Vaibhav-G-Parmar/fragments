28430039a574556739a5b05bbd74b8e040106769
Vaibhav Parmar <vaibhavparmar@virajs-mbp.home>
2023-09-11 19:11:51 -0400

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "commonjs": true,
+//         "es2021": true
+//     },
+//     "extends": "eslint:recommended",
+//     "overrides": [
+//         {
+//             "env": {
+//                 "node": true
+//             },
+//             "files": [
+//                 ".eslintrc.{js,cjs}"
+//             ],
+//             "parserOptions": {
+//                 "sourceType": "script"
+//             }
+//         }
+//     ],
+//     "parserOptions": {
+//         "ecmaVersion": "latest"
+//     },
+//     "rules": {
+//     }
+// }
+
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
