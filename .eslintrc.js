185a412d880382991bd9734c90dcc27a4c5d40de
Vaibhav Parmar <vaibhavparmar@virajs-mbp.home>
2023-09-25 17:24:54 -0400

    updating CI workflow for unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,13 @@
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "commonjs": true,
-//         "es2021": true
-//     },
-//     "extends": "eslint:recommended",
-//     "overrides": [
-//         {
-//             "env": {
-//                 "node": true
-//             },
-//             "files": [
-//                 ".eslintrc.{js,cjs}"
-//             ],
-//             "parserOptions": {
-//                 "sourceType": "script"
-//             }
-//         }
-//     ],
-//     "parserOptions": {
-//         "ecmaVersion": "latest"
-//     },
-//     "rules": {
-//     }
-// }
+// .eslintrc.js
 
 module.exports = {
   env: {
     commonjs: true,
     es2021: true,
     node: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
   overrides: [],
---
+++ tests/unit/app.test.js
@@ -0,0 +1,11 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe("GET requests for undefine routes", () => {
+    test('to get 404 error for undefined routes', () =>
+      request(app).get('/undefined-route').expect(404)
+    );
+});
