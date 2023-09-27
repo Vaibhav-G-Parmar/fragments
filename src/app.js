7d5f9e3bc36e315bd9d3adc16a134f07a836dfc6
Vaibhav Parmar <vaibhavparmar@virajs-mbp.home>
2023-09-27 00:25:29 -0400

    lab 3 done - solved my Internal server error in app.js file, forgot to add curley braces in the require statement

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet');
 const compression = require('compression');
 const passport = require('passport');
 const authenticate = require('./auth');
-const createErrorResponse = require('./response') 
+const { createErrorResponse } = require('./response') 
 
 // // author and version from our package.json file
 // const { author, version } = require('../package.json');
@@ -62,8 +62,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // We may already have an error response we can use, but if not,
   // use a generic `500` server error and message.
-  const status = err.status || 404;
-  const message = err.message || 'unable to process request';
+  const status = err.error.code || 500;
+  const message = err.error.message || 'unable to process request';
 
   // If this is a server error, log something so we can see what's going on.
   if (status > 499) {
