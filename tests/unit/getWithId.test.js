eb881c02f0721b4562e0004295db6447bf0727c1
Vaibhav Parmar <vgparmar@myseneca.ca>
2023-11-10 11:40:08 -0500

    A2-adda a test case in getWithId.test.js

--- tests/unit/getWithId.test.js
+++ tests/unit/getWithId.test.js
@@ -9,8 +9,17 @@ describe('GET /v1/fragments/:id', () => {
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments/:id').expect(401));
 
   // If the wrong username/password pair are used (no such user), it should be forbidden
-  test('incorrect credentials are denied', () =>
-    request(app).get('/v1/fragments/:id').auth('invalid@email.com', 'incorrect_password').expect(401));
+  test('incorrect credentials are denied', () => request(app).get('/v1/fragments/:id').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  //testing that authenticated users can get a plain text fragment
+  //Using a valid username/password pair should give a success result 
+  test('authenticated users get a fragment with the given id', async () => {
+    const postRes = await request(app).post('/v1/fragments').send('fragment with text').set('Content-Type', 'text/plain').auth('user1@email.com', 'password1');
+    const id = (JSON.parse(postRes.text)).fragment.id;
+    const getRes = await request(app).get(`/v1/fragments/${id}`).auth('user1@email.com', 'password1');
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.text).toBe('fragment with text');
+  });
 
   // unknown fragment id should return 404
   test('unknown fragment id returns 404', async () => {
