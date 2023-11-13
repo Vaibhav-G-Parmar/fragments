2515f48d00bdb74579c01bfe14ad33270ddd91c2
Vaibhav Parmar <vgparmar@myseneca.ca>
2023-11-13 02:25:34 -0500

    A2-impvoed test cases for post.test.js

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -12,27 +12,33 @@ describe('POST /fragments', () => {
   test('incorrect credentials are denied', () =>
   request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
+  // Using a valid username/password pair should give a success result with a .fragments array
+  test('authenticated users get a fragments array', async () => {
+    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
+    expect(Array.isArray(res.body.fragments)).toBe(true);
+  });
+
+  //testing that authenticated users cannot create a fragment without any data being sent
+  test('fragment without data will error', async () => {
+    const res = await request(app).post('/v1/fragments').send().auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(500);
+  });
+
   //testing that authenticated users can create a plain text fragment
   //Using a valid username/password pair should give a success result 
   test('authenticated users post a fragment', async () => {
     const res = await request(app).post('/v1/fragments').send('fragment with text').set('Content-type', 'text/plain').auth('user1@email.com', 'password1');
     expect(res.statusCode).toBe(201);
-    expect(res.body.status).toBe('ok');
+    expect(res.type).toBe('text/plain');
   });
-
-  //responses include all necessary and expected properties (id, created, type, etc), 
-  //and these values match what you expect for a given request (e.g., size, type, ownerId)
-  test('responses include all necessary and expected properties', async () => {
-    const res = await request(app).post('/v1/fragments').send('fragment with text').set('Content-type', 'text/plain').auth('user1@email.com', 'password1');
-    const fragment = await readFragment(res.body.fragment.ownerId, res.body.fragment.id);
-    expect(res.body.fragment).toEqual(fragment);
-  });  
 
   //responses include a Location header with a URL to GET the fragment
   test('responses include a Location header with a URL to GET the fragment', async () => {
     const res = await request(app).post('/v1/fragments').send('fragment with a text').set('Content-Type', 'text/plain').auth('user1@email.com', 'password1')
     expect(res.statusCode).toBe(201);
-    expect(res.headers.location).toEqual(`${process.env.API_URL}/v1/fragments/${res.body.fragment.id}`);
+    expect(res.headers.location).toEqual(`${process.env.API_URL}/v1/fragments/${JSON.parse(res.text).fragment.id}`);
   });
 
   //trying to create a fragment with an unsupported type should 
